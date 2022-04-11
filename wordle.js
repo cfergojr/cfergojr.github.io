3e82e65140ed2641f0bf59d91b211f1eebbb634d
Christopher Fergo <cfergojr@gmail.com>
2022-04-11 15:13:32 -0400

    Add files via upload

--- wordle.js
+++ wordle.js
@@ -5,7 +5,7 @@ const messageDisplay = document.querySelector('.message-container')
 let wordle
 
 const getWordle = () => {
-    fetch('/word')
+    fetch('http://localhost:8000/word')
         .then(response => response.json())
         .then(json => {
             wordle = json.toUpperCase()
@@ -115,7 +115,7 @@ const deleteLetter = () => {
 const checkRow = () => {
     const guess = guessRows[currentRow].join('')
     if (currentTile > 4) {
-    fetch('/check/?word=${guess}')
+    fetch('http://localhost:8000/check/?word=${guess}')
         .then(response => response.json())
         .then(json => {
             if (json == 'Entry word not found') {
