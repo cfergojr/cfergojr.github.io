100fd6b1a432163aef99aca537ba4c17a72933b6
Christopher Fergo <cfergojr@gmail.com>
2022-04-11 21:18:55 -0400

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -5,7 +5,7 @@ const messageDisplay = document.querySelector('.message-container')
 let wordle
 
 const getWordle = () => {
-    fetch('http://localhost:8000/word')
+    fetch('/word')
         .then(response => response.json())
         .then(json => {
             wordle = json.toUpperCase()
@@ -115,7 +115,7 @@ const deleteLetter = () => {
 const checkRow = () => {
     const guess = guessRows[currentRow].join('')
     if (currentTile > 4) {
-    fetch('http://localhost:8000/check/?word=${guess}')
+    fetch('/check/?word=${guess}')
         .then(response => response.json())
         .then(json => {
             if (json == 'Entry word not found') {
